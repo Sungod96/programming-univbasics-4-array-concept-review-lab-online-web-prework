74be03efcb2f77555598a1248a59bcfd3f35f49d
Amon-Ra Sanders <mrlive2k@gmail.com>
2020-03-01 20:35:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,8 @@
 def find_element_index(array, value_to_find)
 array.length.times do |count|
-  if array[count] == value_to_find
+  if array[count] == vlaue_to_find
     return count
-end
+  end
 end
 nil
 end
