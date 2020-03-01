34b0049e0bcf554ee5ada10031275daa2b5a053d
Amon-Ra Sanders <mrlive2k@gmail.com>
2020-03-01 20:51:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,10 +12,10 @@ def find_max_value(array)
  max_number = 0 
  array.length.times do |count|
    if array[count] > max_number
-     max_number = array[count]
-    end
-  end
-    return max_number
+     array[count] =  max_number
+   end
+ end
+ return max_number
  end
 
 def find_min_value(array)
